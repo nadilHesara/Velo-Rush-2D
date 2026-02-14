6552bfcfe930c8fc917b22c8816fc68cfb13d25d
nadilHesara <nadilhesara@gmail.com>
2026-02-15 00:06:36 +0530

    Code player basic run and jump functions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public Rigidbody2D rb;
+
+    [Header("Movement Info")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    private bool runBegun;
+
+    [Header("Collision Info")]
+    public float groundCheckDistance;
+    public LayerMask whatIsGround;
+    private bool isGrounded;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +26,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (runBegun)
+            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+
+        CheckCollision();
+
+        CheckInput();
+
+    }
+
+    private void CheckCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetButtonDown("Fire2"))
+            runBegun = true;
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
